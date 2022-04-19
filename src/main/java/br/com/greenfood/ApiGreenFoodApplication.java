e57c5030fca5bb3ab776a81c12d718efb5ba98c8
Adriano Stankewicz <adriano.stankewicz@gmail.com>
2022-04-19 09:34:22 -0300

    Test API

--- src/main/java/br/com/greenfood/ApiGreenFoodApplication.java
+++ src/main/java/br/com/greenfood/ApiGreenFoodApplication.java
@@ -2,11 +2,20 @@ package br.com.greenfood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
-@RestController
 public class ApiGreenFoodApplication {
+	
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World";
+	}
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGreenFoodApplication.class, args);
